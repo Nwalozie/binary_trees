6b3729220266087883cd1c10b0140dffa76bb8cf
Nwalozie <iyekcnwalozie@gmail.com>
2023-08-30 15:30:34 -0700

    Binary Tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -38,6 +38,12 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	return (nodes == pow(2, height + 1) - 1);
 }
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Return: Height of the binary tree, 0 if tree is NULL
+ */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
