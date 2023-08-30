2dae0a264b6cab2aa9b23a0408a6017a1b389ec3
Nwalozie <iyekcnwalozie@gmail.com>
2023-08-30 14:50:04 -0700

    is full

--- binary_trees.h
+++ binary_trees.h
@@ -44,5 +44,6 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
 
 #endif
