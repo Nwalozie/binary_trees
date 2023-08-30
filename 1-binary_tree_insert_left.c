50fd06338ec135500d8e67bf00ecc688291bb6be
Nwalozie <iyekcnwalozie@gmail.com>
2023-08-30 15:23:20 -0700

    Binary Tree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,6 +24,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->right = NULL;
 	new_node->left = NULL;
+
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
