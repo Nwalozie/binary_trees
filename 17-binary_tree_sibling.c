0685ea7133f3a083a37b9731246d1e90aeb4b4a5
Nwalozie <iyekcnwalozie@gmail.com>
2023-08-31 05:21:33 -0700

    Binary Tree

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -12,12 +12,10 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	binary_tree_t *parent = node->parent;
-
-	if (parent->left == node)
-		return (parent->right);
-	else if (parent->right == node)
-		return (parent->left);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else if (node->parent->right == node)
+		return (node->parent->left);
 	else
 		return (NULL);
 }
